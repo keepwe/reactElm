e8841e893511f0ede5458db9e310282f0e4771d2
keepwe <1374854656@qq.com>
2018-09-03 20:19:44 +0800

    file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render(){
     return(
       <div>
-        <h1>hello</h1>
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Elm from './Elm';
-import Footer from './components/Footer';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Elm />, document.getElementById('root'));
